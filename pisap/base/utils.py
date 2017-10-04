838283009c047abe3c8ec5e83d9f78fcc62ef272
Hanae Carrie <hanae.carrie@free.fr>
2017-10-04 15:29:48 +0200

    [Fix]: Adaptation of some methods to non squarred matrix

--- pisap/base/utils.py
+++ pisap/base/utils.py
@@ -19,32 +19,34 @@ def convert_mask_to_locations(mask):
 
     Parameters:
     -----------
-        mask: np.ndarray, {0,1} 2D matrix
+        mask: np.ndarray, {0,1} 2D matrix, not necessarly a square matrix
     Returns:
     -------
         samples_locations: np.ndarray,
     """
     row, col = np.where(mask==1)
     row = row.astype('float') / mask.shape[0] - 0.5
-    col = col.astype('float') / mask.shape[0] - 0.5
+    col = col.astype('float') / mask.shape[1] - 0.5
     return np.c_[row, col]
 
 
-def convert_locations_to_mask(samples_locations, img_size):
+def convert_locations_to_mask(samples_locations, img_shape):
     """ Return the converted the sampling locations as Cartesian mask.
 
     Parameters:
     -----------
         samples_locations: np.ndarray,
-        img_size: int, size of the desired square mask
+        img_shape: tuple of int, shape of the desired mask, not necessarly
+        a square matrix
     Returns:
     -------
         mask: np.ndarray, {0,1} 2D matrix
     """
     samples_locations = samples_locations.astype('float')
     samples_locations += 0.5
-    samples_locations *= img_size
+    samples_locations[:,0] *= img_shape[0]
+    samples_locations[:,1] *= img_shape[1]
     samples_locations = samples_locations.astype('int')
-    mask = np.zeros((img_size, img_size))
+    mask = np.zeros(img_shape)
     mask[samples_locations[:,0], samples_locations[:,1]] = 1
     return mask
